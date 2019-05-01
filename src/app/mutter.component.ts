96366fa7b45a581d3e6d00d29d81ac182b0eb025
Buchner72 <franz.buchner@gmx.at>
2019-05-01 12:41:26 +0200

    Container Rahmen

--- src/app/mutter.component.ts
+++ src/app/mutter.component.ts
@@ -1,26 +1,32 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-mutter',
+  selector: "app-mutter",
   template: `
-   <h1>
-    {{title}}
-   </h1>
+    <div class="container">
+      <div class="bs-docs-example">  <!--styles.css-->
+        <h1>
+          {{ title }}
+        </h1>
+        <div>
+          <div></div>
+        </div>
+      </div>
+    </div>
   `,
   styles: []
 })
 export class MutterComponent {
-  title: string  ;
+  title: string;
 
   constructor() {
-    this.title = 'Hallo Welt!';
-    setTimeout(() => {          // Schreibweide: Innerhalb dieser Funktion soll das 'this' von aussen erhaten bleiben
-     this.title = '2,5 Sekunden sind vorbei';
+    this.title = "Hallo Welt!";
+    setTimeout(() => {
+      // Schreibweide: Innerhalb dieser Funktion soll das 'this' von aussen erhaten bleiben
+      this.title = "2,5 Sekunden sind vorbei";
     }, 2500);
   }
 }
 
 // So wurde diese Componet erstellt
 // ng g c mutter --flat -is -it --spec false
-
-
